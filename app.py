1eddb65422e5c45d912135e39bf6ff984da69db8
luizkazimm <luizcasemiro@kazimm.com.br>
2026-09-13 16:49:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ elif opcao == "Novo Lançamento":
             "Droga Raia",
         ]
 
-        for forn in df_fornecedores_cad["discricao"]:
+        for forn in df_fornecedores_cad["descricao"]:
             if forn not in estabelecimentos_frequentes:
                 estabelecimentos_frequentes.append(forn)
 
